c13cbc3429f884d4259844dd649b0a3346dfa7e5
Chetan <cshariwal3198@gmail.com>
2024-03-15 17:15:04 +0530

    trial intergration for firebase

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,7 +1,8 @@
 import { memo, useCallback, useState } from "react";
 import { useForm } from "react-hook-form";
+import { ErrorMessage } from "../utils/error-message";
 
-const styleString = 'bg-slate-300 p-4 text-xl rounded-xl text-black';
+const styledClassString = 'bg-slate-300 p-4 text-xl rounded-xl text-black';
 
 export const AuthenticationForm = memo(({ onSubmit }: { onSubmit: (values: any) => void }) => {
 
@@ -20,7 +21,7 @@ export const AuthenticationForm = memo(({ onSubmit }: { onSubmit: (values: any) 
 
     const onClick = useCallback(() => setSignedInUser(!signedInUser), [signedInUser]);
 
-    const renderFooterContent = useCallback(() => (
+    const renderAlternateForm = useCallback(() => (
         <div className="my-6 mx-2">
             {signedInUser ? 'New to Netflix? ' : 'Already A User? '}
             <button onClick={onClick}>
@@ -32,34 +33,40 @@ export const AuthenticationForm = memo(({ onSubmit }: { onSubmit: (values: any) 
     ), [onClick, signedInUser]);
 
     return (
-        <div className="flex flex-col gap-4 h-96 w-1/4 text-white self-center bg-zinc-700 bg-transparent p-10 min-h-[550px] min-w-[450px] rounded-xl">
+        <div className="flex flex-col gap-4 w-1/4 text-white self-center bg-zinc-700 bg-transparent p-10 min-h-[550px] min-w-[450px] rounded-xl">
             <span className="font-semibold text-4xl">
                 {
                     signedInUser ? 'Sign In' : 'Sign Up'
                 }
             </span>
             <div className="flex flex-col gap-6 my-4">
-                {
-                    signedInUser ? null :
-                        <input className={styleString} type="text" placeholder="Enter User name" {...register('userName', { required: true, min: 8 })} />
-                }
                 <div className="gap-2 flex flex-col">
-                    <input className={styleString} type="text" placeholder="Enter email" {...register('email', { required: true, validate: validateEmail })} />
                     {
-                        errors?.email ? <span className="text-sm font-medium text-red-600">{errors.email.message as any}</span> : null
+                        signedInUser ? null :
+                            <input className={styledClassString} type="text" placeholder="Enter User name" {...register('userName', { required: true, min: 8 })} />
+                    }
+                    {
+                        signedInUser ? null :
+                            errors?.userName ? <ErrorMessage errorInfo={errors?.userName as any} fieldName="Username" /> : null
                     }
                 </div>
                 <div className="gap-2 flex flex-col">
-                    <input className={styleString} type="password" placeholder="Enter Password" {...register('password', { required: true, min: 8, validate: validatePassword })} />
+                    <input className={styledClassString} type="text" placeholder="Enter email" {...register('email', { required: true, validate: validateEmail })} />
                     {
-                        errors?.password ? <span className="text-sm font-medium text-red-600">{errors.password.message as any}</span> : null
+                        errors?.email ? <ErrorMessage errorInfo={errors?.email as any} fieldName="Email" /> : null
+                    }
+                </div>
+                <div className="gap-2 flex flex-col">
+                    <input className={styledClassString} type="password" placeholder="Enter Password" {...register('password', { required: true, min: 8, validate: validatePassword })} />
+                    {
+                        errors?.password ? <ErrorMessage errorInfo={errors?.password as any} fieldName="Password" /> : null
                     }
                 </div>
             </div>
             <button className="bg-red-600 p-4 text-xl rounded-xl font-semibold" onClick={handleSubmit(onSubmit)}>
                 {signedInUser ? 'Sign In' : 'Sign Up'}
             </button>
-            {renderFooterContent()}
+            {renderAlternateForm()}
         </div>
     )
 });
