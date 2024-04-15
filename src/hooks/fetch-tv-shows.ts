42f9e22baa58a080cc329b76e21d021b190df59b
Chetan <cshariwal3198@gmail.com>
2024-04-15 17:48:52 +0530

    chore: added shows screen

--- src/hooks/fetch-tv-shows.ts
+++ src/hooks/fetch-tv-shows.ts
@@ -5,10 +5,14 @@ const fetcher = (url: string) => (fetch(url, options).then((res) => (res.json())
 
 export const useFetchTvShows = () => {
 
-    const { data, isLoading } = useSWR('https://api.themoviedb.org/3/trending/tv/day?language=en-US', fetcher);
+    const tvShows = useSWR('https://api.themoviedb.org/3/trending/tv/day?language=en-US', fetcher);
+    const popular = useSWR('https://api.themoviedb.org/3/tv/popular?language=en-US&page=1', fetcher);
+    const topRated = useSWR('https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1', fetcher);
+    const details = useSWR('https://api.themoviedb.org/3/tv/106379?language=en-US', fetcher);
 
+    console.log(details.data);
     return {
-        tvShows: data?.results, isLoading
+        trending: tvShows, popular, topRated
     }
 
 }
