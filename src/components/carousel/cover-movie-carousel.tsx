3babb0dd6cd6eebf3130f7355071bb919198a4c7
Chetan <cshariwal3198@gmail.com>
2024-04-17 16:30:17 +0530

    chore: added carousel element

---
+++ src/components/carousel/cover-movie-carousel.tsx
@@ -0,0 +1,96 @@
+import React, { ReactNode, memo, useEffect, useRef, useState } from "react";
+import "./carousel.css";
+import { IMovie } from "../../types";
+import { CoverMovie } from "../cover-movie";
+
+export const CarouselItem = ({ children, width }: { children: ReactNode, width?: string }) => {
+  return (
+    <div className="carousel-item" style={{ width: width }}>
+      {children}
+    </div>
+  );
+};
+
+const Carousel = ({ children }: { children: ReactNode }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const swipe = useRef({});
+
+  const updateIndex = (newIndex: number) => {
+    if (newIndex >= React.Children.count(children) || newIndex < 0) {
+      newIndex = 0;
+    }
+    setActiveIndex(newIndex);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      updateIndex(activeIndex + 1);
+    }, 2000);
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, [activeIndex]);
+
+  const onTouchStart = (e) => {
+    const touch = e.touches[0];
+    swipe.current = { x: touch.clientX };
+  };
+
+  const onTouchMove = (e) => {
+    if (e.changedTouches && e.changedTouches.length) {
+      swipe.current.swiping = true;
+    }
+  };
+
+  const onTouchEnd = (e) => {
+    const touch = e.changedTouches[0];
+    const swipedLeft = touch.clientX - swipe.current.x > 0 ? true : false;
+    const swipedRight = touch.clientX - swipe.current.x > 0 ? false : true;
+    const absX = Math.abs(touch.clientX - swipe.current.x);
+    if (swipe.current.swiping && absX > 50) {
+      if (swipedLeft) {
+        updateIndex(activeIndex - 1);
+      } else if (swipedRight) {
+        updateIndex(activeIndex + 1);
+      }
+    }
+    swipe.current = {};
+  };
+
+  return (
+    <div
+      className="carousel"
+      onTouchStart={onTouchStart}
+      onTouchMove={onTouchMove}
+      onTouchEnd={onTouchEnd}
+    >
+      <div
+        className="inner"
+        style={{ transform: `translateX(-${activeIndex * 100}%)` }}
+      >
+        {React.Children.map(children, (child) => {
+          return React.cloneElement(child, { width: "100%" });
+        })}
+      </div>
+    </div>
+  );
+};
+
+const CoverMovieCarousel = memo(({ movies }: { movies: IMovie[] }) => {
+
+  return (
+    <Carousel>
+      {
+        movies.map((item) => (
+          <CarouselItem key={item.id}>
+            <CoverMovie movieItem={item} />
+          </CarouselItem>
+        ))
+      }
+    </Carousel>
+  )
+});
+
+export default CoverMovieCarousel;
